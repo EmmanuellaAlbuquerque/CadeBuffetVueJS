6df775f6c2518e4723be4007ab8f2feb17558779
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-05-11 20:56:57 -0300

    Adiciona tela inicial com listagem de buffets

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 const app = Vue.createApp({
+  data() {
+    return {
+      buffets: []
+    }
+  },
 
+  async mounted() {
+    await this.getBuffets();
+  },
+
+  methods: {
+    async getBuffets() {
+      let response = await fetch('http://localhost:3000/api/v1/buffets');
+      this.buffets = await response.json();
+    }
+  }
 });
 
 app.mount('#app');
