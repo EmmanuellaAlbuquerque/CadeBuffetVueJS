b2a0e595e072d06ca18120233efba79c9af5b15e
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-05-11 23:55:39 -0300

    Adiciona consulta de disponibilidade para realização de um evento

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
       query: '',
       buffets: [],
       buffet: null,
-      events: []
+      events: [],
+      formData: {
+        event_date: '',
+        qty_invited: ''
+      }
     }
   },
 
@@ -41,6 +45,19 @@ const app = Vue.createApp({
     async getEvents(buffet_id) {
       let response = await fetch(`http://localhost:3000/api/v1/buffets/${buffet_id}/events`);
       this.events = await response.json(); 
+    },
+
+    async checkEventAvailability(event_id) {
+      event.preventDefault();
+      if (!this.formData.event_date || !this.formData.qty_invited) {
+        alert('Por favor, preencha todos os campos obrigatórios.');
+        return;
+      }
+
+      let response = await fetch(`http://localhost:3000/api/v1/events/${event_id}/available?event_date=${this.formData.event_date}&qty_invited=${this.formData.qty_invited}`);
+      const found_event = this.events.find(event => event.id === event_id);
+      
+      found_event.availability_response = await response.json();
     }
   }
 });
