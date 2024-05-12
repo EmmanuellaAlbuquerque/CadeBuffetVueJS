4e2da8224cb8b9ae9324edc58ded956b5469d351
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2024-05-12 19:30:21 -0300

    Adiciona estilização com tailwindcss

--- main.js
+++ main.js
@@ -35,6 +35,10 @@ const app = Vue.createApp({
   },
 
   methods: {
+    async resetBuffet() {
+      this.buffet = null;
+    },
+
     async getBuffets() {
       let response = await fetch('http://localhost:3000/api/v1/buffets');
       this.buffets = await response.json();
@@ -68,6 +72,12 @@ const app = Vue.createApp({
       const found_event = this.events.find(event => event.id === event_id);
       
       found_event.availability_response = await response.json();
+      const date_Parts = this.formData.event_date.split('-');
+      found_event.event_date = date_Parts[2] + '/' + date_Parts[1] + '/' + date_Parts[0];
+      found_event.qty_invited = this.formData.qty_invited;
+
+      this.formData.event_date = '';
+      this.formData.qty_invited = '';
     }
   }
 });
